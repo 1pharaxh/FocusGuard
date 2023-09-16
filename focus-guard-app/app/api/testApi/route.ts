ea45aa49d22b74c2608f20d50a025c5eed38a383
Akarshan Mishra <93630550+1pharaxh@users.noreply.github.com>
2023-09-16 15:54:08 -0600

    FEAT: checking for userIDs

--- focus-guard-app/app/api/testApi/route.ts
+++ focus-guard-app/app/api/testApi/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectToDatabase, SendToAI } from "@/lib";
+import { connectToDatabase, SendToAI, AddToDatabase } from "@/lib";
 // IMPORTANT: THIS IS HOW YOU MAKE AN API : https://www.youtube.com/watch?v=O-NGENb6LNg
 export const GET = async (req: Request, res: Response) => {
   return NextResponse.json({ message: "Hello from the API!" });
@@ -10,9 +10,18 @@ export const POST = async (req: Request, res: Response) => {
   if (!body) {
     return NextResponse.json({ message: "no body!" });
   }
-  const category = await SendToAI(body.page_title);
-  console.log(body.page_title + "-", category);
-  connectToDatabase();
+  // const category = await SendToAI(body.page_title, [
+  //   "Gaming",
+  //   "Politcal News",
+  //   "Other",
+  // ]);
+  // console.log(body.page_title + "-", category);
+  const category = "Other";
+  AddToDatabase(
+    "IAMANID",
+    "10000 KILL SQUAD WIPED TWIN TOWER FORTNITE MONTAGE EPIC 2021 KILLS | SUICDE SQUAD | CASTORS CURSE",
+    ["Gaming", "Others", "Political News", "United Kingdom", "Canada", "USA"]
+  );
 
   return NextResponse.json({ category: category });
 };
--- focus-guard-app/lib/index.ts
+++ focus-guard-app/lib/index.ts
@@ -5,7 +5,7 @@ import { MongoClient } from "mongodb";
 const uri = process.env.NEXT_PUBLIC_MONGODB_URI; // Connection string from MongoDB Atlas
 
 let cachedDb: MongoClient;
-
+// Function that connects to the database and returns the cached database connection
 export async function connectToDatabase() {
   if (!uri) {
     throw new Error("Please define the MONGODB_URI environment variable");
@@ -22,7 +22,8 @@ export async function connectToDatabase() {
   return cachedDb;
 }
 
-export async function SendToAI(text: String) {
+// This is the function that gets a string and returns the category of the string
+export async function SendToAI(text: String, categories: String[]) {
   const openai = new OpenAI({
     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, // This is also the default, can be omitted
   });
@@ -35,11 +36,32 @@ export async function SendToAI(text: String) {
         role: "user",
         content:
           "You can only reply with one of the following answers: " +
-          "['Gaming', 'Politcal News', 'Other'], how would you categorize the following title: '" +
+          JSON.stringify(categories) +
+          ", how would you categorize the following title: '" +
           text +
           "'",
       },
     ],
   });
   return chatCompletion.choices[0].message.content;
+}
+
+// This is function that accepts, userId: a string, page_title: a string, category: a list of strings
+export async function AddToDatabase(
+  userId: string,
+  page_title: string,
+  categories: string[]
+) {
+  // go into database "data"
+  const db = await connectToDatabase();
+  const database = db.db("data");
+  // find a collection that is userId
+  const collection = database.collection(userId);
+  // check if collection exists
+  const exsits = await collection.findOne({ userId: userId });
+  if (exsits) {
+    console.log("Collection exists");
+  } else {
+    console.log("Collection does not exist");
+  }
 }
