740be9aac69a3a3ed55241b75d05b50ccdec1921
Tanta1um <tammy.q.young@gmail.com>
2023-09-16 14:34:28 -0600

    feat: mongo db connection

--- focus-guard-app/app/api/testApi/route.ts
+++ focus-guard-app/app/api/testApi/route.ts
@@ -1,7 +1,33 @@
 import OpenAI from "openai";
 import { NextResponse } from "next/server";
+
+import express, { Request, Response } from "express";
+import { ObjectId } from "mongodb";
+import { collections } from "../../../services/database.service";
+import { connectToDatabase } from "../../../services/database.service";
+
+export const router = express.Router();
+router.use(express.json());
+
+const app = express();
+const port = 8080; // default port to listen
+
+connectToDatabase()
+    .then(() => {
+        app.use("/api/testApi", router);
+
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+        });
+    })
+    .catch((error: Error) => {
+        console.error("Database connection failed", error);
+        process.exit();
+    });
+
 // IMPORTANT: THIS IS HOW YOU MAKE AN API : https://www.youtube.com/watch?v=O-NGENb6LNg
 export const GET = async (req: Request, res: Response) => {
+  
   return NextResponse.json({ message: "Hello from the API!" });
 };
 
@@ -12,6 +38,7 @@ export const POST = async (req: Request, res: Response) => {
   }
   const category = await openai(body.page_title);
   console.log(body.page_title + "-", category);
+
   return NextResponse.json({ category: category });
 };
 
