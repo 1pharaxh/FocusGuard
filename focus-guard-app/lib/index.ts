e4d1a00a8f1cf14719090f127ab63c1b8a8a13df
Akarshan Mishra <93630550+1pharaxh@users.noreply.github.com>
2023-09-17 11:16:17 -0600

    FEAT: added new frontend lib

--- focus-guard-app/lib/index.ts
+++ focus-guard-app/lib/index.ts
@@ -243,7 +243,6 @@ export async function CheckIfCategoriesExist(userId: string) {
     doc.userId &&
     doc.CurrentCategories &&
     doc.AllCategories &&
-    doc.currentUser &&
     doc.AllCategories.length > 1
   ) {
     return true;
